8fd3a972b42454f6407e9fe53098810b4988458e
Samsun Sung <samsun.sung@gmail.com>
2026-01-04 17:48:11 +0900

    edit

---
+++ types.ts
@@ -0,0 +1,13 @@
+
+export interface ServiceItem {
+  id: number;
+  title: string;
+  description: string;
+  features: string[];
+  icon: string;
+}
+
+export interface NavLink {
+  name: string;
+  href: string;
+}
